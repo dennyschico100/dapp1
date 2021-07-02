5282db317538307bbbed508b1bcb01cc4e4b676e
dennyschico100 <dennyschico2@gmail.com>
2021-07-02 14:26:03 -0600

    primer commit

---
+++ src/contracts/SocialNetwork.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+contract    SocialNetwork{
+        string public name;    
+
+    constructor() public {
+        name ="A DAPP APPLICATION";
+    }
+
+}
